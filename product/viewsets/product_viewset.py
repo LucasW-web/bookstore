66497222db03a1091fdd4ea0fc0bb6d215d2d3db
Lucas <lucas_wayne2023@hotmail.com>
2026-06-29 16:27:20 -0300

    adicionando a paginação com o DRF

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
@@ -8,7 +8,7 @@ from product.serializers.product_serializer import ProductSerializer
 class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
 
     def get_queryset(self):
         return Product.objects.all().order_by("id")
